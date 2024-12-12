2a98fc1e17a100866057ef53f755f88905781d79
Maaa1k <m.a.lopez.bosch@gmail.com>
2024-12-12 16:10:17 +0100

    room id 3 conexion OK

--- src/main/java/com/malb/maze/controller/GameController.java
+++ src/main/java/com/malb/maze/controller/GameController.java
@@ -3,7 +3,9 @@ package com.malb.maze.controller;
 import com.malb.maze.model.Room;
 import com.malb.maze.service.RoomService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GameController {
@@ -16,19 +18,21 @@ public class GameController {
     // Método para manejar el GET de /dashboard
     @GetMapping("/dashboard")
     public String showDashboard() {
-        Room room = roomService.getRoom(3);
+       /* Room room = roomService.getRoom(3);
         room.setIdMaze(1);
         room.setItemCoin(0);
 
-        System.out.println("VALUE DE ROOM N: " + room.getN());
+        System.out.println("VALUE DE ROOM N: " + room.getN());*/
         return "dashboard";  // Retorna el nombre de la plantilla (sin la extensión .html)
     }
 
     // Método para manejar el GET de /dashboard
-    @GetMapping("/start")
-    public String showStart() {
+    @PostMapping("/roomview")
+    public String showStart(Model model) {
+        Room room = roomService.getRoom(3);
+        model.addAttribute("rooms", room);
         // Aquí, Thymeleaf buscará el archivo dashboard.html en src/main/resources/templates
-        return "start";  // Retorna el nombre de la plantilla (sin la extensión .html)
+        return "roomview";  // Retorna el nombre de la plantilla (sin la extensión .html)
     }
 
 }
